0e2f3b65b888e6ca6ece28608b20c6c953a585f1
yirongwn <yirongwn@pjd-rz>
2024-03-15 15:33:53 +0000

    update all statistics for all traces

--- batch_run_statistics.sh
+++ batch_run_statistics.sh
@@ -2,7 +2,7 @@
 # Title: Run statistics.py for traces with name pattern "IBMObjectStoreTraceXXXPartX" files
 # Description: This script runs the statistics.py script for all traces with the name pattern "IBMObjectStoreTrace000Part0" to "IBMObjectStoreTrace097Part9", missing files are skipped.
 
-for i in {11..20}; do
+for i in {61..97}; do
     for j in {0..9}; do
         file="IBMObjectStoreTrace$(printf '%03d' $i)Part$j"
         if [ -f "$file" ]; then
